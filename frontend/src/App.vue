26734305d3ccbeff47f5f45ebeb8802c36529564
Chupa <amalik.moussaoui1710@gmail.com>
2025-10-25 12:37:36 +0200

    Sets up initial Vue.js project structure
    Initializes the Vue.js project with necessary components:
    - Establishes basic routing using Vue Router, including routes for connection and dashboard views.
    - Configures the main application component and mounting process.
    - Sets up a basic project structure.

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
+
+<style>
+  body {
+    margin: 0;
+  }
+</style>
