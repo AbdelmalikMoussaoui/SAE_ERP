3c2286c7095c9bc70d5b031eb38ab6e716180086
Chupa <amalik.moussaoui1710@gmail.com>
2025-11-25 14:32:26 +0100

    Add GestionUtilisateurs view and update router configuration

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ConnexionView from '../views/Connexion.vue'
-// 1. Importez votre nouveau dashboard
-import DashboardView from '../views/Dashboard.vue'
+import DashboardView from '../views/Dashboard.vue' 
+import GestionUtilisateurs from '../views/GestionUtilisateurs.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,15 +11,22 @@ const router = createRouter({
       name: 'connexion',
       component: ConnexionView
     },
+    
     {
       path: '/',
       redirect: '/connexion'
     },
-    // 2. Ajoutez la nouvelle route ici
+    
     {
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView
+    },
+
+    {
+      path: '/gestion-utilisateurs',
+      name: 'gestion-utilisateurs',
+      component: GestionUtilisateurs
     }
   ]
 })
